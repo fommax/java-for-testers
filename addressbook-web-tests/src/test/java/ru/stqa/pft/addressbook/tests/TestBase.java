f6ac97fb9d85ed8a7bf92dca2aaf4026e660c647
Maksim Fomin <cold_soviet_steel@mail.ru>
2018-06-27 00:10:53 +0200

    Реализована поддержка Google Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -14,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
 
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
   FirefoxDriver wd;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
