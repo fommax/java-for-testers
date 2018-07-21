111a2172dfed40d2e0f0fbec0e584b42c7d0eaa8
Maksim Fomin <cold_soviet_steel@mail.ru>
2018-07-22 00:34:37 +0200

    Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -10,7 +10,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
   FirefoxDriver wd;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
